de20b3476ce130d0e118854ae9046f0febdbab64
iibrahim70 <iibrahiim.dev@gmail.com>
2024-06-27 21:22:09 +0600

    :lipstick: added basic navbar

--- src/components/shared/Navbar.tsx
+++ src/components/shared/Navbar.tsx
@@ -1,5 +1,11 @@
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav className="py-3 shadow-md">
+      <div className="section-wrapper">
+        <h6>Rate Sync</h6>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
